9c8fe2ef33f12e7b50d93df68e877318a3fd602b
Abin Thomas <followgamer102@gmail.com>
2025-06-27 21:59:08 +0530

    feat: Added doctor ui functions

--- actions/doctor.ts
+++ actions/doctor.ts
@@ -1,14 +1,58 @@
 "use server";
 
 import { db } from "@/db";
-import { patients } from "@/db/schema";
+import { patients, doctors } from "@/db/schema";
 
-export const addDoctor = (formData: Record<string, string>) => {}
+interface DoctorFormData {
+    firstName: string;
+    lastName: string;
+    phoneNumber: string;
+    gender: string;
+    email: string;
+    password: string;
+}
+export const addDoctor = (formData: DoctorFormData) => {
+    try{
+    return db.insert(doctors).values({
+        fullName: `${formData.firstName} ${formData.lastName}`,
+        phoneNumber: formData.phoneNumber,
+        gender: formData.gender,
+        email: formData.email,
+        password: formData.password,
+    });
+    } catch (error) {
+        console.error("Error adding doctor:", error);
+        throw new Error("Failed to add doctor");
+    }
 
-export const getDoctor = (id: string) => {}
+}
 
-export const getTotalAppointments = (id: string) => {}
+import { eq } from "drizzle-orm";
 
-export const getPatientsList = (id: string) => {}
+export const getDoctor = (id: string) => {
+    try {
+        return db.select()
+            .from(doctors)
+            .where(eq(doctors.id, id))
+            .then(results => results[0]);
+    } catch (error) {
+        console.error("Error fetching doctor:", error);
+        throw new Error("Failed to fetch doctor");
+    }
+}
 
-export const getPatientReports = (id: string) => {}
+export const getPatientsList = (id: string) => {
+    try {
+        return db.select()
+            .from(patients)
+            .where(eq(patients.doctor, id))
+            .then(results => results);
+    } catch (error) {
+        console.error("Error fetching patients list:", error);
+        throw new Error("Failed to fetch patiensts list");
+    }
+}
+
+export const getPatientReports = (id: string) => {
+
+}
