0d1c2821df4029dcf457f7838531bb532a7e57ae
Abin Thomas <followgamer102@gmail.com>
2025-06-27 11:49:23 +0530

    fix: header updated

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import Dashboard from "../Component/custom/dashboard";
 export default function Home() {
   return (
-    <div>
+    <div className=" p-5 flex flex-col items-center justify-center h-screen bg-gradient-to-r from-slate-200 via-slate-300 to-slate-400">
       <Dashboard />
     </div>
   );
